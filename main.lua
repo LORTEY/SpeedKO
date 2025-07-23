37975b072f0967fd953bd843bcf5d15c75c0dcba
LORTEY <wnozka19@gmail.com>
2025-07-23 19:30:54 +0200

    Finally found lines

--- main.lua
+++ main.lua
@@ -9,6 +9,7 @@ local speedko = WidgetContainer:extend({
     name = "speedko",
     is_doc_only = false,
 })
+local Blitbuffer = require("ffi/blitbuffer")
 local Device = require("device")
 local Screen = Device.screen
 function speedko:onDispatcherRegisterActions()
@@ -24,7 +25,22 @@ function speedko:init()
     self:onDispatcherRegisterActions()
     self.ui.menu:registerToMainMenu(self)
 end
+function dump(var, depth)
+    depth = depth or 0
+    local indent = string.rep("  ", depth)
 
+    if type(var) == "table" then
+        local s = "{\n"
+        for k, v in pairs(var) do
+            s = s .. indent .. "  [" .. tostring(k) .. "] = " .. dump(v, depth + 1) .. ",\n"
+        end
+        return s .. indent .. "}"
+    elseif type(var) == "string" then
+        return '"' .. var .. '"'
+    else
+        return tostring(var)
+    end
+end
 function speedko:addToMainMenu(menu_items)
     local rect = {
         x = 137,
@@ -32,11 +48,39 @@ function speedko:addToMainMenu(menu_items)
         w = 170,
         y = 166,
     }
+    local highlight_data = {
+
+        [1] = {
+            {
+                colorful = true,
+                drawer = "lighten",
+                color = Blitbuffer.ColorRGB32(255, 255, 51, 255),
+                index = 2,
+                rect = {
+                    y = 287,
+                    w = 180,
+                    h = 27,
+                    x = 154,
+                },
+            },
+        },
+    }
+    local pos0 = { x = 0, y = 0 }
+    local pos1 = { x = Screen:getWidth(), y = Screen:getHeight() }
     menu_items.speedko_menu = {
         text = _("Speed Reading Settings"),
         sorting_hint = "setting",
+
         callback = function()
-            self.ui.view.drawHighlightRect(self.ui.view, Screen.bb, 0, 0, rect, "lighten")
+            -- self.ui.view.drawHighlightRect(self.ui.view, Screen.bb, 0, 0, rect, "lighten")
+            self.ui.view.visible_boxes = highlight_data
+            self.ui.view:drawSavedHighlight(Screen.bb, 0, 0)
+            local word0 = self.ui.document:getWordFromPosition(pos0, true).pos0
+            local word1 = self.ui.document:getWordFromPosition(pos1, true).pos1
+
+            logger.dbg(
+                "Lortey\n\n\n\n\n\n" .. dump(self.ui.document._document:getWordBoxesFromPositions(word0, word1, false))
+            )
             UIManager:show(InfoMessage:new({
                 text = _("Speed reading settings menu"), -- Fixed: Added proper text
             }))
@@ -44,9 +88,4 @@ function speedko:addToMainMenu(menu_items)
     }
 end
 
-function speedko:onSpeedkoEvent()
-    UIManager:show(InfoMessage:new({
-        text = _("Speed reading mode activated"), -- Fixed: Added proper text
-    }))
-end
 return speedko
